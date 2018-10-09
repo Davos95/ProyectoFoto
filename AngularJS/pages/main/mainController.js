c0698b7e0b974e9bb3868eec4002379599e31df9
Davos <davidvalencia_@hotmail.com>
2018-10-09 14:25:13 +0200

    Initial commit

---
+++ AngularJS/pages/main/mainController.js
@@ -0,0 +1,24 @@
+﻿app.controller("mainController", function ($scope, $route) {
+
+    function aparecer(elemento, tiempo) {
+        var elem = document.getElementById(elemento);
+        var opacity = 0;
+        elem.style.opacity = opacity;
+        setTimeout(function () { var id = setInterval(frame, 2); }, tiempo);
+
+        function frame() {
+            if (opacity >= 1) {
+                clearInterval(this);
+            } else {
+                opacity += 0.01;
+                elem.style.opacity = opacity;
+            }
+        }
+    }
+    aparecer("subirImagen", 100);
+    aparecer("editarImagen", 200);
+    aparecer("crearSesion", 400);
+    aparecer("editarSesion", 600);
+    aparecer("crearComision", 800);
+    aparecer("editarComision", 1000);
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+ProyectoFoto
